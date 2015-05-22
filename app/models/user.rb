f686bbbfd984cc22135b0b786a5ceb2ae0ea6d65
Nikhat Bhayani <nikhataly@gmail.com>
2015-05-21 21:00:44 -0400

    Started validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   has_many :projects, through: :memberships
   has_many :projects
   belongs_to :personality
+
+  validates_confirmation_of :password
+  validates_presence_of :password, :on => :create
+  validates_presence_of :email
+  validates_uniqueness_of :email
+
+  validates :email, uniqueness: true
 end
