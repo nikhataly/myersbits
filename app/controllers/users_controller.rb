b5d1aca7366f79b6156f4757a68fc5cd925a30a3
Nikhat Bhayani <nikhataly@gmail.com>
2015-05-21 20:42:44 -0400

    Show method to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,7 @@ class UsersController < ApplicationController
 
   def create
   end
+
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :memberships, only: [:new, :create, :destroy, :index]
   end
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
