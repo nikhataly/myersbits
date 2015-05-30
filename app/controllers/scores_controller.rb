c7661477a84c18ec49e7d313651d9d58f8e3219e
Nikhat Bhayani <nikhataly@gmail.com>
2015-05-30 18:49:54 -0400

    Score calculations seem to work ok now

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -8,22 +8,16 @@ class ScoresController < ApplicationController
   end
 
   def create
-    binding.pry
     answers = (params[:answers].values.unshift(0)).map{|ans| ans.to_i}
-    binding.pry
     ie = Score.calculate_ie(answers)
     sn = Score.calculate_sn(answers)
     ft = Score.calculate_ft(answers)
     jp = Score.calculate_jp(answers)
-    binding.pry
     @score = Score.new(extraversion: ie, openness: sn, agreeableness: ft, conscientiousness: jp)
     @score.user = current_user
-    binding.pry
     current_user.personality = Personality.find_by_mbti(@score.mbti)
     current_user.save
-    binding.pry
     @score.save
-    binding.pry
   end
 
   def destroy
