e2ae3ead464772ec2bcb85d0a5090d285339fe78
Nikhat Bhayani <nikhataly@gmail.com>
2015-06-08 17:26:11 -0400

    Score

--- app/models/personality.rb
+++ app/models/personality.rb
@@ -1,10 +1,13 @@
 class Personality < ActiveRecord::Base
-
-
   has_many :users
   has_many :recommendations
   has_many :relationships, foreign_key: :primary_id
   has_many :personalities, through: :relationships, source: :secondary
+
+  # def self.find_mbti(mbti)
+  #   find_by(mbti: mbti.to_s.upcase)
+  #   # Personality.find_by_mbti(mbti.to_s.upcase)
+  # end
 
   def self.[](mbti)
     find_by(mbti: mbti.to_s.upcase)
