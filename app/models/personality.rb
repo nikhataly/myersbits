32c0a834322fa5cd5f32275d7115622cb21328c3
Nikhat Bhayani <nikhataly@gmail.com>
2015-06-09 17:47:40 -0400

    more stuff

--- app/models/personality.rb
+++ app/models/personality.rb
@@ -3,6 +3,7 @@ class Personality < ActiveRecord::Base
   has_many :recommendations
   has_many :relationships, foreign_key: :primary_id
   has_many :personalities, through: :relationships, source: :secondary
+  serialize :description
 
   # def self.find_mbti(mbti)
   #   find_by(mbti: mbti.to_s.upcase)
