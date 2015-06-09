4a94ddc1d511f4638a8e06c5ac99f26f233c6b19
Nikhat Bhayani <nikhataly@gmail.com>
2015-06-09 14:06:09 -0400

    users index

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :new, :create, :show] do
     get :autocomplete_skill_name, :on => :collection
-    
+
   end
 
   resources(:compatibilities, only: [:index, :show])
